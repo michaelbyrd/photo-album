44879ab4f0e960e480a0241d6c9be0405424249a
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-03-11 17:19:10 -0400

    removed repetative code

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,8 +1,5 @@
 class Photo < ActiveRecord::Base
   validates :title, presence: true
-  has_attached_file :upload,
-    :storage => :s3,
-    :s3_credentials => {:bucket => ENV['S3_BUCKET'], :access_key_id => ENV['S3_KEY_ID'], :secret_access_key => ENV['S3_SECRET_KEY']}
+  has_attached_file :upload
   validates_attachment_content_type :upload, :content_type => /\Aimage\/.*\Z/
-
 end
