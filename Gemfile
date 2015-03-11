d59966c466e22ef295e0eb333adb7d35a371ca61
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-03-11 13:12:23 -0400

    add gem paperclip, generate photo model

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'paperclip'
+
 gem 'bourbon'
 gem 'neat'
 
---
+++ db/migrate/20150311170943_create_photos.rb
@@ -0,0 +1,10 @@
+class CreatePhotos < ActiveRecord::Migration
+  def change
+    create_table :photos do |t|
+      t.string :title
+
+      t.timestamps null: false
+    end
+    add_attachment :photos, :upload
+  end
+end
